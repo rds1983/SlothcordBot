6c5c27a17ce37e5f8b0f02eae5c6558b1eb995c8
Roman Shapiro <eleroman@mail.ru>
2023-03-24 21:51:20 +0700

    Some refactoring

--- AlertsProcessor.ts
+++ AlertsProcessor.ts
@@ -15,8 +15,55 @@ class Event {
 	time: string;
 }
 
+class EventParseInfo {
+	type: EventType;
+	regex: RegExp;
+	doerFirst: boolean;
+
+	constructor(type: EventType, regex: string, doerFirst: boolean) {
+		this.type = type;
+		this.regex = new RegExp(regex, "s");
+		this.doerFirst = doerFirst;
+	}
+}
+
 export class AlertsProcessor extends BaseProcessorImpl<Event[]>
 {
+	private static readonly DeathParsers: EventParseInfo[] =
+		[
+			new EventParseInfo(EventType.Death, "(.+) handidly dispatched (\\w+) to to the next world\\.", true),
+			new EventParseInfo(EventType.Death, "(.+) mercilessly slaughtered (\\w+)\\.", true),
+			new EventParseInfo(EventType.Death, "(.+) mercilessly butchered (\\w+)\\.", true),
+			new EventParseInfo(EventType.Death, "(.+) obliterated (\\w+)\\.", true),
+			new EventParseInfo(EventType.Death, "(.+) annihilated (\\w+)\\.", true),
+			new EventParseInfo(EventType.Death, "(.+) defeated (\\w+)\\.", true),
+			new EventParseInfo(EventType.Death, "(.+) slew (\\w+)\\.", true),
+			new EventParseInfo(EventType.Death, "(.+) wasted (\\w+)\\.", true),
+			new EventParseInfo(EventType.Death, "(.+) crushed (\\w+) to a liveless pulp of blood and offals\\.", true),
+			new EventParseInfo(EventType.Death, "(\\w+) was slain by (.+)\\.", false),
+			new EventParseInfo(EventType.Death, "(\\w+) was defeated by (.+)\\.", false),
+			new EventParseInfo(EventType.Death, "(\\w+) was messily dispatched by (.+)\\.", false),
+			new EventParseInfo(EventType.Death, "(\\w+) was beaten down by (.+)\\.", false),
+			new EventParseInfo(EventType.Death, "(\\w+) naively fought (.+) and lost\\.", false),
+			new EventParseInfo(EventType.Death, "(\\w+) fought against (.+) and lost\\.", false)
+		];
+
+	private static readonly RaiseParsers: EventParseInfo[] =
+		[
+			new EventParseInfo(EventType.Raise, "(\\w+) sold a piece of soul to the devil in exchange for (\\w+)'s worthless soul", true),
+			new EventParseInfo(EventType.Raise, "The clerical genius (\\w+) successfully raised (\\w+)\\.", true),
+			new EventParseInfo(EventType.Raise, "(\\w+)'s prayers were answered and (\\w+) was successfully raised\\.", true),
+			new EventParseInfo(EventType.Raise, "(\\w+) raised from the dead by (\\w+)\\.", false)
+		];
+
+	private static readonly ShockParsers: EventParseInfo[] =
+		[
+			new EventParseInfo(EventType.Shock, "(\\w+) shocked (\\w+)\\.", true),
+			new EventParseInfo(EventType.Shock, "(\\w+) knelt, prayed, and still managed to shock (\\w+)\\.", false),
+			new EventParseInfo(EventType.Shock, "(\\w+) was banished to ether by (\\w+)'s lack of raising ability\\.", false),
+			new EventParseInfo(EventType.Shock, "The gods liked (\\w+)'s soul so much that they want to keep it\\s+\\-\\s+(\\w+) was not convincing enough to cheat death.", false)
+		];
+
 	constructor(client: Client) {
 		super(client);
 	}
@@ -29,13 +76,12 @@ export class AlertsProcessor extends BaseProcessorImpl<Event[]>
 		return 30 * 1000;
 	}
 
-	private static processEvent(type: EventType, regex: string, text: string, time: string, newEvents: Event[], doerFirst: boolean): boolean {
-		let re = new RegExp(regex, "s");
-		let m = re.exec(text);
+	private static processEventWithParser(parser: EventParseInfo, text: string, time: string, newEvents: Event[]): boolean {
+		let m = parser.regex.exec(text);
 		if (m) {
 			let adventurer: string;
 			let doer: string;
-			if (doerFirst) {
+			if (parser.doerFirst) {
 				adventurer = m[2];
 				doer = m[1];
 			} else {
@@ -45,7 +91,7 @@ export class AlertsProcessor extends BaseProcessorImpl<Event[]>
 
 			let newEvent: Event =
 			{
-				type: type,
+				type: parser.type,
 				adventurer: adventurer,
 				doer: doer,
 				time: time
@@ -54,6 +100,16 @@ export class AlertsProcessor extends BaseProcessorImpl<Event[]>
 			newEvents.push(newEvent);
 
 			return true;
+		}
+
+		return false;
+	}
+
+	private static processEventWithParsers(parsers: EventParseInfo[], text: string, time: string, newEvents: Event[]): boolean {
+		for (let i = 0; i < parsers.length; ++i) {
+			if (AlertsProcessor.processEventWithParser(parsers[i], text, time, newEvents)) {
+				return true;
+			}
 		}
 
 		return false;
@@ -130,37 +186,10 @@ export class AlertsProcessor extends BaseProcessorImpl<Event[]>
 			let time = row.childNodes[0].textContent.trim();
 			let eventText = row.childNodes[1].textContent.trim();
 
-			// Check if it's death
-			if (!AlertsProcessor.processEvent(EventType.Death, "(.+) handidly dispatched (\\w+) to to the next world\\.", eventText, time, newEvents, true) &&
-				!AlertsProcessor.processEvent(EventType.Death, "(.+) mercilessly slaughtered (\\w+)\\.", eventText, time, newEvents, true) &&
-				!AlertsProcessor.processEvent(EventType.Death, "(.+) mercilessly butchered (\\w+)\\.", eventText, time, newEvents, true) &&
-				!AlertsProcessor.processEvent(EventType.Death, "(.+) obliterated (\\w+)\\.", eventText, time, newEvents, true) &&
-				!AlertsProcessor.processEvent(EventType.Death, "(.+) annihilated (\\w+)\\.", eventText, time, newEvents, true) &&
-				!AlertsProcessor.processEvent(EventType.Death, "(.+) defeated (\\w+)\\.", eventText, time, newEvents, true) &&
-				!AlertsProcessor.processEvent(EventType.Death, "(.+) slew (\\w+)\\.", eventText, time, newEvents, true) &&
-				!AlertsProcessor.processEvent(EventType.Death, "(.+) wasted (\\w+)\\.", eventText, time, newEvents, true) &&
-				!AlertsProcessor.processEvent(EventType.Death, "(.+) crushed (\\w+) to a liveless pulp of blood and offals\\.", eventText, time, newEvents, true) &&
-				!AlertsProcessor.processEvent(EventType.Death, "(\\w+) was slain by (.+)\\.", eventText, time, newEvents, false) &&
-				!AlertsProcessor.processEvent(EventType.Death, "(\\w+) was defeated by (.+)\\.", eventText, time, newEvents, false) &&
-				!AlertsProcessor.processEvent(EventType.Death, "(\\w+) was messily dispatched by (.+)\\.", eventText, time, newEvents, false) &&
-				!AlertsProcessor.processEvent(EventType.Death, "(\\w+) was beaten down by (.+)\\.", eventText, time, newEvents, false) &&
-				!AlertsProcessor.processEvent(EventType.Death, "(\\w+) naively fought (.+) and lost\\.", eventText, time, newEvents, false) &&
-				!AlertsProcessor.processEvent(EventType.Death, "(\\w+) fought against (.+) and lost\\.", eventText, time, newEvents, false)) {
-
-				// Check if it's raise
-				if (!AlertsProcessor.processEvent(EventType.Raise, "(\\w+) sold a piece of soul to the devil in exchange for (\\w+)'s worthless soul", eventText, time, newEvents, true) &&
-					!AlertsProcessor.processEvent(EventType.Raise, "The clerical genius (\\w+) successfully raised (\\w+)\\.", eventText, time, newEvents, true) &&
-					!AlertsProcessor.processEvent(EventType.Raise, "(\\w+)'s prayers were answered and (\\w+) was successfully raised\\.", eventText, time, newEvents, true) &&
-					!AlertsProcessor.processEvent(EventType.Raise, "(\\w+) raised from the dead by (\\w+)\\.", eventText, time, newEvents, false)) {
-
-					// Finally check if it's shock
-					if (!AlertsProcessor.processEvent(EventType.Shock, "(\\w+) shocked (\\w+)\\.", eventText, time, newEvents, true) &&
-						!AlertsProcessor.processEvent(EventType.Shock, "(\\w+) knelt, prayed, and still managed to shock (\\w+)\\.", eventText, time, newEvents, false) &&
-						!AlertsProcessor.processEvent(EventType.Shock, "(\\w+) was banished to ether by (\\w+)'s lack of raising ability\\.", eventText, time, newEvents, false) &&
-						!AlertsProcessor.processEvent(EventType.Shock, "The gods liked (\\w+)'s soul so much that they want to keep it\\s+\\-\\s+(\\w+) was not convincing enough to cheat death.", eventText, time, newEvents, false)) {
-						this.logInfo(`'${eventText}' neither death or raise or shock.`);
-					}
-				}
+			if (!AlertsProcessor.processEventWithParsers(AlertsProcessor.DeathParsers, eventText, time, newEvents) &&
+				!AlertsProcessor.processEventWithParsers(AlertsProcessor.RaiseParsers, eventText, time, newEvents) &&
+				!AlertsProcessor.processEventWithParsers(AlertsProcessor.ShockParsers, eventText, time, newEvents)) {
+				this.logInfo(`'${eventText}' neither death or raise or shock.`);
 			}
 		}
 
