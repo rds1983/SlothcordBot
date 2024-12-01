09f3ddb1ff8cbf497ae2c1bb8f7e55db633eb5f8
Roman Shapiro <eleroman@mail.ru>
2024-12-02 02:48:51 +0700

    Missing file

---
+++ Constants.ts
@@ -0,0 +1,8 @@
+export class Constants {
+	public static readonly AlertsUrl = "http://www.slothmud.org/wp/live-info/live-blog";
+	public static readonly EmporiumUrl = "http://www.slothmud.org/wp/live-info/live-auctions";
+	public static readonly EpicsUrl = "http://www.slothmud.org/support/mapserver2.php?filter=all";
+	public static readonly ForumUrl = "http://www.slothmud.org/wp/";
+	public static readonly GroupsUrl = "http://www.slothmud.org/wp/live-info/adventuring-parties"
+}
+
